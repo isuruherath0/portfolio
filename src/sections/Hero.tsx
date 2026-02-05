32cd381c930430166231c92f3a9f3dac59b3202f
Isuru Herath <isuruherath0@gmail.com>
2026-02-05 16:59:48 +0530

    Hero Page v1

--- src/sections/Hero.tsx
+++ src/sections/Hero.tsx
@@ -1,5 +1,26 @@
-export default function Hero(){
-    return(
-        <h1>Hero</h1>
-    )
+'use client';
+
+import  { ReactTyped } from 'react-typed';
+
+export default function Hero() {
+  return (
+    <main className="min-h-screen flex items-center justify-center bg-white text-black">
+      <div className="flex flex-col">
+        <h1 className="text-2xl">
+          <ReactTyped
+            strings={['Hi, I\'m Isuru Herath']}
+            typeSpeed={40}
+            showCursor={false}
+          />
+        </h1>
+        <h1 className="text-2xl">
+            <ReactTyped
+            strings={['Let\'s Get to Know Me Better']}
+            typeSpeed={40}
+            startDelay={2000}
+          />
+        </h1>
+      </div>
+    </main>
+  );
 }
--- src/sections/Projects.tsx
+++ src/sections/Projects.tsx
@@ -1,6 +1,8 @@
 
 export default function Projects() {
   return (
-    <div>Projects</div>
+   <main className="min-h-screen flex items-center justify-center bg-white text-black">
+    <h1>Projects</h1>
+   </main>
   )
 }
